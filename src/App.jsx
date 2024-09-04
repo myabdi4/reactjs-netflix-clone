1627f226f772443f747d780cacdef1e63d138671
Abdirahman Nur <myabdi4@gmail.com>
2024-09-04 01:51:35 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,13 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 import Player from "./Pages/Player/Player.jsx";
 import { onAuthStateChanged } from "firebase/auth";
 import { useEffect } from "react";
-import { auth, db } from "./firebase/firebase.jsx";
-import { ToastContainer, toast } from "react-toastify";
+import { auth } from "./firebase/firebase.jsx";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Profiles from "./Pages/Profiles/Profiles.jsx";
 
-// TEST CODE
-import { doc, getDoc } from "firebase/firestore";
-// import { doc, getDoc } from "firebase/firestore";
-// import { auth, db } from "./firebase"; // Adjust the path as needed
-
 function App() {
   const navigate = useNavigate();
-  const user = auth.currentUser;
-  // console.log(user.name);
 
   useEffect(() => {
     onAuthStateChanged(auth, async (user) => {
